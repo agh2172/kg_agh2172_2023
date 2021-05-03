5984700c0b7f10157046a0bee02dd57249141ef5
Axel <axel_henrikson@asl.org>
2021-05-03 15:50:36 -0400

    Finished project

--- main.js
+++ main.js
@@ -6,16 +6,31 @@ for(i=0; i<myArgs.length; i++){
     var numString = num.toString();
     var j;
     for(j=0; j<numString.length; j++){
-	console.log("made it into for loop2!!!");
 	var digit = numString.substring(j,j+1);
-	console.log(digit + ' wooo');
 	if(digit=='0'){
 	    output = output+'Zero';
 	}else if(digit=='1'){
 	    output = output+'One';
 	}else if(digit=='2'){
 	    output = output+'Two';
-	}
+	}else if(digit==='3'){
+	    output = output+'Three';
+	}else if(digit==='4'){
+            output = output+'Four';
+        }else if(digit==='5'){
+            output = output+'Five';
+        }else if(digit==='6'){
+            output = output+'Six';
+        }else if(digit==='7'){
+            output = output+'Seven';
+        }else if(digit==='8'){
+            output = output+'Eight';
+        }else if(digit==='9'){
+            output = output+'Nine';
+        }
     }
+    output=output+',';
 }
-console.log(output);
+if(output.length>0){
+    console.log(output.substring(0,output.length-1));
+}
